95e95944902dcb52c62d15212608fb81c74a69f8
Ilan Ganor <ilan@kamacode.com>
2021-01-04 17:35:08 +0200

    Read the bytes from SFC User Data Area -- Prepare for Serial Number report.

--- versionmsg.cpp
+++ versionmsg.cpp
@@ -7,10 +7,46 @@
 #include <QJsonObject>
 #include <QJsonArray>
 
+
+//TODO remove unused:
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/ioctl.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
 //NOTE: Next header is used for random()
 //TODO: replace with QRandomGenerator, when passing to qt 5.12
 
+#define	AT91C_BASE_SFC	0xf804c000
+#define	AT91C_BASE_PMC	0xf0014000
+
+#define	AT91C_SFC_DR0_OFFSET (0x20)
+#define	AT91C_PMC_PCR_OFFSET (0x10c)
+
+//Fuse Controller ID:
+#define	AT91C_ID_SFC 50
+#define	AT91C_PMC_PCR_EN (1 << 28)
+#define	AT91C_PMC_PCR_CMD (1 << 12)
+
+
+
 void VersionMsg::singleShot(CanManager *aCanManager)
+{
+    sendVersionInfo(aCanManager);
+    sendServiceNumber(aCanManager);
+}
+
+
+
+void VersionMsg::sendVersionInfo(CanManager *aCanManager)
 {
     //NOTE: Engine version:
     struct can_frame frame_to_send;
@@ -70,10 +106,83 @@ void VersionMsg::singleShot(CanManager *aCanManager)
         frame_to_send.data[5] = (quint8)(0xff);
         frame_to_send.data[6] = (quint8)(0xff);
         frame_to_send.data[7] = (quint8)(0xff);
-
-
     }
 
-    aCanManager->write_frame(&frame_to_send);
+    aCanManager->write_frame(&frame_to_send);   
 }
+
+void VersionMsg::sendServiceNumber(CanManager *aCanManager)
+{
+    //NOTE: Engine version:
+    struct can_frame frame_to_send_LSB;
+    struct can_frame frame_to_send_MSB;
+
+
+    QJsonObject jsonObject = AMJsonConfigReader::getInstance()->object();
+    QJsonArray jsonArray = jsonObject["MediaVersion"].toArray();
+
+    frame_to_send_LSB.can_id = 0x7d1;
+    frame_to_send_LSB.can_dlc = 8;
+
+    frame_to_send_MSB.can_id = 0x7d2;
+    frame_to_send_MSB.can_dlc = 8;
+
+    //TODO read the SN and verify:
+    qint32 mem_fd = open("/dev/mem",O_RDWR);
+
+    void* pmc_pcr_ptr = mmap(NULL, AT91C_PMC_PCR_OFFSET + sizeof(quint32), PROT_WRITE,
+                        MAP_PRIVATE, mem_fd, AT91C_BASE_PMC);
+
+
+    void* sfc_dr_ptr = mmap(NULL, AT91C_SFC_DR0_OFFSET + 16*sizeof(quint32), PROT_READ,
+                        MAP_PRIVATE, mem_fd, AT91C_BASE_SFC);
+
+
+
+    close(mem_fd);
+
+
+    enableDisableSFC((quint32*)(pmc_pcr_ptr)+(AT91C_PMC_PCR_OFFSET/sizeof(quint32)),true);
+
+
+    for(qint32 i = 0; i<16;i++)
+    {
+        qDebug()<<"sn"<<i<<":"<<readDataSFC((quint32*)sfc_dr_ptr+(AT91C_SFC_DR0_OFFSET/sizeof(quint32)),i);
+    }
+
+
+
+     enableDisableSFC((quint32*)(pmc_pcr_ptr)+(AT91C_PMC_PCR_OFFSET/sizeof(quint32)),false);
+
+
+    //1. if all are 0x00 send the source
+
+    //2. if not verify
+
+    //3. if broken: set all to 0xFF
+
+
+    munmap(sfc_dr_ptr, AT91C_SFC_DR0_OFFSET + 16*sizeof(quint32));
+
+    munmap(pmc_pcr_ptr, AT91C_PMC_PCR_OFFSET + sizeof(quint32));
+
+    //aCanManager->write_frame(&frame_to_send_LSB);
+    //aCanManager->write_frame(&frame_to_send_MSB);
+}
+
+void VersionMsg::enableDisableSFC(quint32* wr_ptr, bool On)
+{
+    *wr_ptr = On ? (AT91C_ID_SFC | AT91C_PMC_PCR_CMD | AT91C_PMC_PCR_EN) :
+                   (AT91C_ID_SFC | AT91C_PMC_PCR_CMD);
+}
+
+quint32 VersionMsg::readDataSFC(quint32* rd_ptr, quint32 index)
+{
+   quint32 ret;
+   ret = *(rd_ptr+index);
+   return ret;
+}
+
+
+
 
--- versionmsg.h
+++ versionmsg.h
@@ -10,6 +10,11 @@ class VersionMsg
 public:
     //Single time fired
     static void singleShot(CanManager * aCanManager);
+private:
+    static void sendVersionInfo(CanManager * aCanManager);
+    static void sendServiceNumber(CanManager * aCanManager);
+    static void enableDisableSFC(quint32 * wr_ptr, bool On);
+    static quint32 readDataSFC(quint32 * rd_ptr, quint32 index);
 };
 
 #endif // VERSIONMSG_H
